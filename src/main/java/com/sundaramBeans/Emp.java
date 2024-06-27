85bf68b753716e5955df2b60031dfa6b91695c86
Sundaram Agnihotri <agnihotrisundaram8@gmail.com>
2024-06-27 18:11:40 +0530

    without orm code structure

---
+++ src/main/java/com/sundaramBeans/Emp.java
@@ -0,0 +1,27 @@
+package com.sundaramBeans;
+
+
+//define the property and behavior of emp class
+public class Emp {
+	private int id;
+	private String name,cmp;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCmp() {
+		return cmp;
+	}
+	public void setCmp(String cmp) {
+		this.cmp = cmp;
+	}
+
+}
